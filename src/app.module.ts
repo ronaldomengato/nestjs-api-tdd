e7426e42f4fe6ae82bb9690e3ed4373d382dabba
Ronaldo Cesar Mengato Junior <ronaldo.mengato@gmail.com>
2022-01-06 21:30:24 -0300

    adds typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+import { join } from 'path';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { IngredientModule } from './ingredient/ingredient.module';
 
 @Module({
-  imports: [IngredientModule],
+  imports: [
+    IngredientModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as any,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
